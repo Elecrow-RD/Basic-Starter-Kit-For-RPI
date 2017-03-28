191c0b383cbc2e392c5e4d9807d29f91bb1fe087
Elecrow-keen <1154001860@qq.com>
2017-03-28 07:22:42 +0000

    test blink.c

---
+++ blink.c
@@ -0,0 +1,26 @@
+/*
+ * Created by keen
+ * Modified by keen 
+ * Date: 28/03/2017
+ */
+
+#include <wiringPi.h>
+#include <stdio.h>
+#define led_pin 0
+
+int main(void){
+
+  printf( "Welcome to Elecrow...\n");
+  printf( "Raspberry Pi blink program...\n" );
+  printf( "Press Ctrl+C to exit\n..." );
+  wiringPiSetup();
+  pinMode(led_pin,OUTPUT); 
+  while(1){
+    digitalWrite(led_pin,HIGH);
+    delay (1000);
+    digitalWrite(led_pin,  LOW);
+    delay (1000);
+  }
+}
+/*******************************************/
+
